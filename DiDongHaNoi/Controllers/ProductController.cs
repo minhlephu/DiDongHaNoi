e25227975d43295b00bcf2613963b6dec12c40fe
minhlephu <110329077+minhlephu@users.noreply.github.com>
2023-04-22 22:33:57 +0700

    Layout

---
+++ DiDongHaNoi/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using DiDongHaNoi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DiDongHaNoi.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly QlbanDienThoaiContext _context;
+        public ProductController(QlbanDienThoaiContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Details(int Id)
+        {
+            var product = _context.Products.Include(x => x.Cat).FirstOrDefault(x => x.ProductId == Id);
+            if(product == null)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+    }
+}
