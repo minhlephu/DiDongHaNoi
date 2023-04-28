20d197f35159a48191aa98fc538b25ec2e486952
minhlephu <110329077+minhlephu@users.noreply.github.com>
2023-04-28 14:25:16 +0700

    Quản lý posts

---
+++ DiDongHaNoi/Areas/Admin/Controllers/AdminPostsController.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DiDongHaNoi.Models;
+using AspNetCoreHero.ToastNotification.Abstractions;
+using PagedList.Core;
+using DiDongHaNoi.Helpper;
+
+namespace DiDongHaNoi.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AdminPostsController : Controller
+    {
+        private readonly QlbanDienThoaiContext _context;
+
+        public INotyfService _notyfService { get; }
+        public AdminPostsController(QlbanDienThoaiContext context, INotyfService notyfService)
+        {
+            _context = context;
+            _notyfService = notyfService;
+        }
+
+        // GET: Admin/AdminPosts
+        public IActionResult Index(int? page)
+        {
+            var collection = _context.Posts.AsNoTracking().ToList();
+            foreach (var item in collection)
+            {
+                if (item.CreateDate == null)
+                {
+                    item.CreateDate = DateTime.Now;
+                    _context.Update(item);
+                    _context.SaveChanges();
+                }
+            }
+
+            var pageNumber = page == null || page <= 0 ? 1 : page.Value;
+            var pageSize = 10;
+            var lsTinDangs = _context.Posts
+                .AsNoTracking()
+                .OrderBy(x => x.PostId);
+            PagedList<Post> models = new PagedList<Post>(lsTinDangs, pageNumber, pageSize);
+
+            ViewBag.CurrentPage = pageNumber;
+            return View(models);
+        }
+
+        // GET: Admin/AdminPosts/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Posts == null)
+            {
+                return NotFound();
+            }
+
+            var post = await _context.Posts
+                .FirstOrDefaultAsync(m => m.PostId == id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+
+            return View(post);
+        }
+
+        // GET: Admin/AdminPosts/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/AdminPosts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("PostId,Title,Scontents,Contents,Thumb,Published,IsNewFeed,IsHot,Alias,CreateDate,Author,AccountId,CatId,Tags,MetaDesc,MetaKey,Views")] Post post, Microsoft.AspNetCore.Http.IFormFile fThumb)
+        {
+            if (ModelState.IsValid)
+            {
+                if (fThumb != null)
+                {
+                    string extension = Path.GetExtension(fThumb.FileName);
+                    string imageName = Utilities.SEOUrl(post.Title) + extension;
+                    post.Thumb = await Utilities.UploadFile(fThumb, @"news", imageName.ToLower());
+                }
+                if (string.IsNullOrEmpty(post.Thumb)) post.Thumb = "default.jpg";
+                post.Alias = Utilities.SEOUrl(post.Title);
+                post.CreateDate = DateTime.Now;
+
+                _context.Add(post);
+                await _context.SaveChangesAsync();
+                _notyfService.Success("Thêm mới thành công");
+                return RedirectToAction(nameof(Index));
+            }
+            return View(post);
+        }
+
+        // GET: Admin/AdminPosts/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Posts == null)
+            {
+                return NotFound();
+            }
+
+            var post = await _context.Posts.FindAsync(id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+            return View(post);
+        }
+
+        // POST: Admin/AdminPosts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("PostId,Title,Scontents,Contents,Thumb,Published,IsNewFeed,IsHot,Alias,CreateDate,Author,AccountId,CatId,Tags,MetaDesc,MetaKey,Views")] Post post)
+        {
+            if (id != post.PostId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(post);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PostExists(post.PostId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(post);
+        }
+
+        // GET: Admin/AdminPosts/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Posts == null)
+            {
+                return NotFound();
+            }
+
+            var post = await _context.Posts
+                .FirstOrDefaultAsync(m => m.PostId == id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+
+            return View(post);
+        }
+
+        // POST: Admin/AdminPosts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Posts == null)
+            {
+                return Problem("Entity set 'QlbanDienThoaiContext.Posts'  is null.");
+            }
+            var post = await _context.Posts.FindAsync(id);
+            if (post != null)
+            {
+                _context.Posts.Remove(post);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PostExists(int id)
+        {
+          return (_context.Posts?.Any(e => e.PostId == id)).GetValueOrDefault();
+        }
+    }
+}
